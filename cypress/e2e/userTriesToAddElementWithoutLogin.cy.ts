d4192cc37e6736227dba2fbd46092108f490d8db
jevshyl <jevgenia.shylovich@edu.tbz.ch>
2025-09-25 13:43:51 +0200

    rename file and add check

--- cypress/e2e/userTriesToAddElementWithoutLogin.cy.ts
+++ cypress/e2e/userTriesToAddElementWithoutLogin.cy.ts
@@ -2,6 +2,6 @@ describe("Try to add list element without login", () => {
 
     it("Should not access the page and get redirected", () => {
         cy.visit("/profileList/0d8fa44c-54fd-4cd0-ace9-2a7da57992de");
-
+        cy.url().should('include', '/login');
     });
 });
