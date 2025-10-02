5602864e9e7c18b114b120db6a28d9988a203066
jevshyl <jevgenia.shylovich@edu.tbz.ch>
2025-10-02 15:13:06 +0200

    add main-workflow and adjust cypress

--- cypress/e2e/userCreatesListElement.cy.ts
+++ cypress/e2e/userCreatesListElement.cy.ts
@@ -6,7 +6,7 @@ describe("Add list element as user", () => {
         cy.get('[data-cy=password]').type("1234");
         cy.get('[data-cy=submit-login]').click();
 
-        cy.request("POST", "http://localhost:8080/user/login", {
+        cy.request("POST", "https://jevgenia.dev.noseryoung.ch/login", {
             email: "user@example.com",
             password: "1234"
         })
